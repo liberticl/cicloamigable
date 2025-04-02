22c65c423f2790496c320629ff864616cd00566c
Francisco Vergara <fvergara@drivetech.pro>
2025-04-02 17:32:51 -0300

    fix(page): se corrigen elementos visuales

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ def get_points(base_url):
             "description": "Estacionamientos instalados en plazas para dejar la bicicleta cuando te encuentres cerca. Son de tipo U invertida con capacidad para un máximo de 10 bicicletas.",  # noqa
             "coordinates": [-71.6309695, -33.0477747],
             "icon": {
-                "url": f"{base_url}static/img/icons/estacionamiento.png",
+                "url": f"{base_url}/static/img/icons/estacionamiento.png",
                 "width": 242,
                 "height": 242,
                 "anchorY": 242,
@@ -28,7 +28,7 @@ def get_points(base_url):
             "description": "Estacionamientos instalados en plazas para dejar la bicicleta cuando te encuentres cerca. Son de tipo U invertida con capacidad para un máximo de 10 bicicletas.",  # noqa
             "coordinates": [-71.6216531, -33.0529473],
             "icon": {
-                "url": f"{base_url}static/img/icons/estacionamiento.png",
+                "url": f"{base_url}/static/img/icons/estacionamiento.png",
                 "width": 242,
                 "height": 242,
                 "anchorY": 242
@@ -39,7 +39,7 @@ def get_points(base_url):
             "description": "Estacionamientos instalados en plazas para dejar la bicicleta cuando te encuentres cerca. Son de tipo U invertida con capacidad para un máximo de 10 bicicletas.",  # noqa
             "coordinates": [-71.6201343, -33.0462627],
             "icon": {
-                "url": f"{base_url}static/img/icons/estacionamiento.png",
+                "url": f"{base_url}/static/img/icons/estacionamiento.png",
                 "width": 242,
                 "height": 242,
                 "anchorY": 242
@@ -50,7 +50,7 @@ def get_points(base_url):
             "description": "Estacionamientos instalados en plazas para dejar la bicicleta cuando te encuentres cerca. Son de tipo U invertida con capacidad para un máximo de 10 bicicletas.",  # noqa
             "coordinates": [-71.5912128, -33.0327061],
             "icon": {
-                "url": f"{base_url}static/img/icons/estacionamiento.png",
+                "url": f"{base_url}/static/img/icons/estacionamiento.png",
                 "width": 242,
                 "height": 242,
                 "anchorY": 242
@@ -61,7 +61,7 @@ def get_points(base_url):
             "description": "Señalética que indica calle compartida entre vehículos motorizados y ciclos. Por regulación, sólo puede utilizarse en zonas 30 y se encuentra acompañada de una demarcación de doble chevron.",  # noqa
             "coordinates": [-71.633855, -33.046935],
             "icon": {
-                "url": f"{base_url}static/img/icons/ciclocalle.png",
+                "url": f"{base_url}/static/img/icons/ciclocalle.png",
                 "width": 242,
                 "height": 242,
                 "anchorY": 242
@@ -72,7 +72,7 @@ def get_points(base_url):
             "description": "Señalética que indica calle compartida entre vehículos motorizados y ciclos. Por regulación, sólo puede utilizarse en zonas 30 y se encuentra acompañada de una demarcación de doble chevron.",  # noqa
             "coordinates": [-71.634301, -33.042629],
             "icon": {
-                "url": f"{base_url}static/img/icons/ciclocalle.png",
+                "url": f"{base_url}/static/img/icons/ciclocalle.png",
                 "width": 242,
                 "height": 242,
                 "anchorY": 242
@@ -83,7 +83,7 @@ def get_points(base_url):
             "description": "Señalética que indica calle compartida entre vehículos motorizados y ciclos. Por regulación, sólo puede utilizarse en zonas 30 y se encuentra acompañada de una demarcación de doble chevron.",  # noqa
             "coordinates": [-71.638566, -33.040495],
             "icon": {
-                "url": f"{base_url}static/img/icons/ciclocalle.png",
+                "url": f"{base_url}/static/img/icons/ciclocalle.png",
                 "width": 242,
                 "height": 242,
                 "anchorY": 242
@@ -94,7 +94,7 @@ def get_points(base_url):
             "description": "Señalética que indica calle compartida entre vehículos motorizados y ciclos. Por regulación, sólo puede utilizarse en zonas 30 y se encuentra acompañada de una demarcación de doble chevron.",  # noqa
             "coordinates": [-71.634407, -33.031686],
             "icon": {
-                "url": f"{base_url}static/img/icons/ciclocalle.png",
+                "url": f"{base_url}/static/img/icons/ciclocalle.png",
                 "width": 242,
                 "height": 242,
                 "anchorY": 242
@@ -105,7 +105,7 @@ def get_points(base_url):
             "description": "Señalética que indica calle compartida entre vehículos motorizados y ciclos. Por regulación, sólo puede utilizarse en zonas 30 y se encuentra acompañada de una demarcación de doble chevron.",  # noqa
             "coordinates": [-71.633268, -33.028571],
             "icon": {
-                "url": f"{base_url}static/img/icons/ciclocalle.png",
+                "url": f"{base_url}/static/img/icons/ciclocalle.png",
                 "width": 242,
                 "height": 242,
                 "anchorY": 242
@@ -116,7 +116,7 @@ def get_points(base_url):
             "description": "Señalética que indica calle compartida entre vehículos motorizados y ciclos. Por regulación, sólo puede utilizarse en zonas 30 y se encuentra acompañada de una demarcación de doble chevron.",  # noqa
             "coordinates": [-71.62225, -33.05473],
             "icon": {
-                "url": f"{base_url}static/img/icons/ciclocalle.png",
+                "url": f"{base_url}/static/img/icons/ciclocalle.png",
                 "width": 242,
                 "height": 242,
                 "anchorY": 242
@@ -127,7 +127,7 @@ def get_points(base_url):
             "description": "Señalética que indica que la vereda se comparte entre peatones y usuarios de ciclos. Fue considerada en este proyecto debido al uso multiprósito que tiene Errázuriz por el ancho del paseo peatonal.",  # noqa
             "coordinates": [-71.620832, -33.04308],
             "icon": {
-                "url": f"{base_url}static/img/icons/peatones-ciclos.png",
+                "url": f"{base_url}/static/img/icons/peatones-ciclos.png",
                 "width": 242,
                 "height": 242,
                 "anchorY": 242
@@ -138,7 +138,7 @@ def get_points(base_url):
             "description": "Señalética que indica que la vereda se comparte entre peatones y usuarios de ciclos. Fue considerada en este proyecto debido al uso multiprósito que tiene Errázuriz por el ancho del paseo peatonal.",  # noqa
             "coordinates": [-71.606211, -33.043238],
             "icon": {
-                "url": f"{base_url}static/img/icons/peatones-ciclos.png",
+                "url": f"{base_url}/static/img/icons/peatones-ciclos.png",
                 "width": 242,
                 "height": 242,
                 "anchorY": 242
@@ -149,7 +149,7 @@ def get_points(base_url):
             "description": "Señalética que indica que la vereda se comparte entre peatones y usuarios de ciclos. Fue considerada en este proyecto debido al uso multiprósito que tiene Errázuriz por el ancho del paseo peatonal.",  # noqa
             "coordinates": [-71.6082922, -33.0439705],
             "icon": {
-                "url": f"{base_url}static/img/icons/peatones-ciclos.png",
+                "url": f"{base_url}/static/img/icons/peatones-ciclos.png",
                 "width": 242,
                 "height": 242,
                 "anchorY": 242
